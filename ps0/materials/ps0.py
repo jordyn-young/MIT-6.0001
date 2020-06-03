8d1ab85311ebc347f776bad2c7a2548a151b8905
Jordyn Young <58190255+jordyn-young@users.noreply.github.com>
2020-06-03 17:46:59 -0400

    Add files via upload

---
+++ ps0/materials/ps0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 20:20:53 2020
+
+@author: jordynyoung
+"""
+
+import math
+
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print(f"x**y = {x ** y}")
+print(f"log(x) = {math.log2(x)}")
